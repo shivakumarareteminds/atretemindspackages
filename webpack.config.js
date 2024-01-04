eb676c5f8cff09e41bd820f775b8e05b6ab94cc5
shivakumarareteminds <shiva.kumar@areteminds.com>
2024-01-04 15:22:37 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // ... other configurations
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
